be01f569b62dfcee1802ce36790c38af77c2f56e
Traceur <traceur759@gmail.com>
2017-09-02 22:43:40 +0200

    Cleared notification lasts for 5 seconds only

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import discord
 from time import sleep
 from random import randint
 
+PREFIX = "dev_"
+TOKEN = "token"
+
 description = '''An example bot to showcase the discord.ext.commands extension
 module.
 There are a number of utility commands being showcased here.'''
@@ -19,27 +22,36 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith("-"):
+    if message.content.startswith(PREFIX):
         global messages
         valid = False
         print("command detected")
-        if message.content == "-clear":
-            msg = await client.send_message(message.channel, 'Clearing...')
-            messages.append(msg)
-            for msg in messages:
-                await client.delete_message(msg)
-            await client.delete_message(message)
-            deleted = len(messages)
-            msg = await client.send_message(message.channel, 'Deleted ' + str(deleted) + ' messages')
-            messages.append(msg)
-            messages = []
-        elif message.content == "-roll":
+        if message.content == PREFIX + "clear":
+            await _clear(message)
+        elif message.content == PREFIX + "roll":
             valid = True
-            rand = randint(1, 6)
-            print("roll called")
-            msg = await client.send_message(message.channel,rand)
-            messages.append(msg)
+            await _roll(message)
         if valid:
             messages.append(message)
 
-client.run('token')
+async def _clear(message):
+    global messages
+    msg = await client.send_message(message.channel, 'Clearing...')
+    messages.append(msg)
+    for msg in messages:
+        await client.delete_message(msg)
+    await client.delete_message(message)
+    deleted = len(messages)
+    msg = await client.send_message(message.channel, 'Deleted ' + str(deleted) + ' messages')
+    messages = []
+    sleep(5)
+    await client.delete_message(msg)
+
+async def _roll(message):
+        global messages
+        rand = randint(1, 6)
+        msg = await client.send_message(message.channel, rand)
+        messages.append(msg)
+
+
+client.run(TOKEN)
